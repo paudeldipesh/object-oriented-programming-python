83a55dd36b8a47daae743a99d6009b46c2862912
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-09 22:22:08 +0545

    add __init__ method

--- classes.py
+++ classes.py
@@ -1,21 +1,16 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name: str, price: float, quantity=1):
+        print(f"Item name: {name}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return float(self.price * self.quantity)
 
 
-item_one = Item()
-item_one.name = "Phone"
-item_one.price = 100
-item_one.quantity = 5
-print(item_one.calculate_total_price(item_one.price, item_one.quantity))
+item_one = Item("Phone", 100, 5)
+print(item_one.calculate_total_price())
 
-item_two = Item()
-item_two.name = "Laptop"
-item_two.price = 1000
-item_two.quantity = 3
-print(item_two.calculate_total_price(item_two.price, item_two.quantity))
-
-random_string = "dipesh"
-print(random_string.upper())
-
-# Defining a method in a class
+item_two = Item("Laptop", 1000)
+print(item_two.calculate_total_price())
