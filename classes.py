fd3a0f7ea27c4696b4960640e197d7f8cada8d30
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-09 18:47:23 +0545

    define a simple method in class

--- classes.py
+++ classes.py
@@ -1,13 +1,21 @@
 class Item:
-    pass
+    def calculate_total_price(self, x, y):
+        return x * y
 
 
 item_one = Item()
 item_one.name = "Phone"
 item_one.price = 100
 item_one.quantity = 5
+print(item_one.calculate_total_price(item_one.price, item_one.quantity))
 
-print(type(item_one))
-print(type(item_one.name))
-print(type(item_one.price))
-print(type(item_one.quantity))
+item_two = Item()
+item_two.name = "Laptop"
+item_two.price = 1000
+item_two.quantity = 3
+print(item_two.calculate_total_price(item_two.price, item_two.quantity))
+
+random_string = "dipesh"
+print(random_string.upper())
+
+# Defining a method in a class
