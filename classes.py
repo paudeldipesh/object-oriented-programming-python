845cd4df2d81b015069b324095c62fcfa6549e75
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-09 18:34:24 +0545

    learn about basic object and class

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Item:
+    pass
+
+
+item_one = Item()
+item_one.name = "Phone"
+item_one.price = 100
+item_one.quantity = 5
+
+print(type(item_one))
+print(type(item_one.name))
+print(type(item_one.price))
+print(type(item_one.quantity))
---
+++ main.py
@@ -0,0 +1,11 @@
+item_one = "Phone"
+item_price = 100
+item_quantity = 5
+item_total_price = item_price * item_quantity
+
+print(type(item_one))  # <class 'str'>
+print(type(item_price))  # <class 'int'>
+print(type(item_quantity))  # <class 'int'>
+print(type(item_total_price))  # <class 'int'>
+
+# Each data type is an object that has been instantiated earlier by some class
